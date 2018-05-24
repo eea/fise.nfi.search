65217666fb17e1b6e90b29b7e649929781289b03
Cornel Nițu <cornel.nitu@eaudeweb.ro>
2018-05-24 12:14:01 +0300

    add migrations

--- Dockerfile
+++ Dockerfile
@@ -2,18 +2,32 @@ FROM python:3.6-slim
 
 ARG REQUIREMENTS_FILE=requirements.dep.txt
 
-ENV PROJ_DIR=/var/local/fise.nfi.search
+ENV APP_HOME=/var/local/fise.nfi.search
 
-RUN runDeps="vim netcat libpq-dev" \
+RUN apt-get update -y \
+    && apt-get install -y --no-install-recommends curl \
+    && curl -sL https://deb.nodesource.com/setup_8.x | bash - \
+    && curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - \
+    && echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+
+RUN runDeps="vim netcat libpq-dev nodejs yarn" \
     && apt-get update -y \
     && apt-get install -y --no-install-recommends $runDeps \
+    && apt-get clean \
     && rm -vrf /var/lib/apt/lists/*
 
-RUN mkdir -p $PROJ_DIR
-COPY requirements.txt $REQUIREMENTS_FILE $PROJ_DIR/
-WORKDIR $PROJ_DIR
+RUN mkdir -p $APP_HOME
+
+COPY yarn.lock package*.json requirements.txt $REQUIREMENTS_FILE $APP_HOME/
+WORKDIR $APP_HOME
 
 RUN pip install --no-cache-dir -r $REQUIREMENTS_FILE
 
-COPY . $PROJ_DIR
+RUN npm_config_tmp=$APP_HOME yarn
+
+COPY . $APP_HOME
+
+RUN npm run build
+
 ENTRYPOINT ["sh", "./docker-entrypoint.sh"]
+CMD ["run"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -15,14 +15,30 @@ if [ -z "$TIMEOUT" ]; then
     TIMEOUT=30
 fi
 
-if [ -z "$1" ]; then
-    python manage.py migrate &&
-    exec gunicorn -e SCRIPT_NAME=$SCRIPT_NAME \
-                  nfi_search.wsgi:application \
-                  --name search \
-                  --bind 0.0.0.0:8000 \
-                  --workers 3 \
-                  --timeout $TIMEOUT \
-                  --access-logfile - \
-                  --error-logfile -
+if [ "x$DJANGO_MIGRATE" = 'xyes' ]; then
+    python manage.py migrate --noinput
 fi
+
+if [ "x$DJANGO_COLLECT_STATIC" = "xyes" ]; then
+  python manage.py collectstatic --noinput
+fi
+
+case "$1" in
+    manage)
+        exec python manage.py "$1"
+        ;;
+    run)
+        if [ "x$DEBUG" = 'xyes' ]; then
+            exec python manage.py runserver 0.0.0.0:${GUNICORN_PORT:-8000}
+        else
+            exec gunicorn -e SCRIPT_NAME=$SCRIPT_NAME \
+                        nfi_search.wsgi:application \
+                        --name search \
+                        --bind 0.0.0.0:${GUNICORN_PORT:-8000} \
+                        --workers 3 \
+                        --timeout $TIMEOUT \
+                        --access-logfile - \
+                        --error-logfile -
+            ;;
+    *)
+esac
