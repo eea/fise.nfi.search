7921c462e89ddf0e05973063bede1c486d74bfe2
Diana Boiangiu <boiangiudiana58@gmail.com>
2018-05-30 11:49:09 +0300

    Supress OneToOneField warning

--- nfi_search/settings.py
+++ nfi_search/settings.py
@@ -24,6 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY', 'secret')
 
+SILENCED_SYSTEM_CHECKS = ["fields.W342"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG', False)
 
