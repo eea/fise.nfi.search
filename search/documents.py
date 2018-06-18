aae7a16742ef50fce1672544ed5117d6827ecb48
Andrei Duhnea <andrei@duhnea.net>
2018-06-18 09:34:56 +0300

    Add fields to doctype; faceted search

--- search/documents.py
+++ search/documents.py
@@ -1,114 +1,115 @@
 from django.conf import settings
 from django_elasticsearch_dsl import DocType, Index, fields
 from elasticsearch_dsl.analysis import analyzer, normalizer
-from elasticsearch_dsl import tokenizer
+from elasticsearch_dsl import tokenizer, FacetedSearch, TermsFacet
 from search.models import Document
 
 
 nfi = Index('nfi')
 nfi.settings(max_result_window=settings.MAX_RESULT_WINDOW)
 
-case_insensitive_analyzer = analyzer(
-    'case_insensitive_analyzer',
-    tokenizer=tokenizer('trigram', 'nGram', min_gram=3, max_gram=3),
-    filter=['lowercase']
-)
-
-case_insensitive_normalizer = normalizer(
-    type="custom",
-    name_or_instance='case_insensitive_normalizer',
+lowercase_normalizer = normalizer(
+    type='custom',
+    name_or_instance='lowercase_normalizer',
     char_filter=[],
-    filter="lowercase",
+    filter='lowercase',
 )
 
 
 @nfi.doc_type
 class DocumentDoc(DocType):
+
     title = fields.TextField(
-        analyzer=case_insensitive_analyzer,
+        analyzer='standard',
+        boost=5.0,
         fielddata=True,
-        fields={'raw': fields.KeywordField(
-                                multi=True,
-                                ignore_above=256,
-                                normalizer=case_insensitive_normalizer
-        )}
     )
-    country = fields.TextField(
-        analyzer=case_insensitive_analyzer,
+
+    description = fields.TextField(
+        analyzer='standard',
+        boost=2.0,
+        fielddata=True,
+    )
+
+    country = fields.KeywordField(
         attr='country.name',
-        fielddata=True,
-        fields={'raw': fields.KeywordField(
-                                attr='country.name',
-                                multi=True,
-                                ignore_above=256,
-                                normalizer=case_insensitive_normalizer
-        )}
+        normalizer=lowercase_normalizer
     )
-    country_id = fields.KeywordField(attr='country.id')
-    data_type = fields.TextField(
-        analyzer=case_insensitive_analyzer,
+
+    data_type = fields.KeywordField(
         attr='data_type.name',
-        fielddata=True,
-        fields={'raw': fields.KeywordField(
-                                attr='data_type.name',
-                                multi=True,
-                                ignore_above=256,
-                                normalizer=case_insensitive_normalizer
-        )}
+        normalizer=lowercase_normalizer
     )
-    data_set = fields.TextField(
-        analyzer=case_insensitive_analyzer,
+
+    data_set = fields.KeywordField(
         attr='data_set.name',
-        fielddata=True,
-        fields={'raw': fields.KeywordField(
-                                attr='data_set.name',
-                                multi=True,
-                                ignore_above=256,
-                                normalizer=case_insensitive_normalizer
-        )}
+        normalizer=lowercase_normalizer
     )
-    resource_type = fields.TextField(
-        analyzer=case_insensitive_analyzer,
-        attr='resource_type.name',
-        fielddata=True,
-        fields={'raw': fields.KeywordField(
-                                attr='resource_type.name',
-                                multi=True,
-                                ignore_above=256,
-                                normalizer=case_insensitive_normalizer
-        )}
+
+    data_source = fields.KeywordField(
+        attr='data_source.name',
+        normalizer=lowercase_normalizer
     )
-    info_level = fields.TextField(
-        analyzer=case_insensitive_analyzer,
+
+    info_level = fields.KeywordField(
         attr='info_level.name',
-        fielddata=True,
-        fields={'raw': fields.KeywordField(
-                                attr='info_level.name',
-                                multi=True,
-                                ignore_above=256,
-                                normalizer=case_insensitive_normalizer
-        )}
+        normalizer=lowercase_normalizer
     )
-    topic_category = fields.TextField(
-        analyzer=case_insensitive_analyzer,
+
+    resource_type = fields.KeywordField(
+        attr='resource_type.name',
+        normalizer=lowercase_normalizer
+    )
+
+    topic_category = fields.KeywordField(
         attr='topic_category.name',
-        fielddata=True,
-        fields={'raw': fields.KeywordField(
-                                attr='topic_category.name',
-                                multi=True,
-                                ignore_above=256,
-                                normalizer=case_insensitive_normalizer
-        )}
+        normalizer=lowercase_normalizer
     )
+
+    keywords = fields.NestedField(
+        properties={
+            'name': fields.KeywordField(normalizer=lowercase_normalizer),
+        }
+    )
+
+    nuts_levels = fields.NestedField(
+        properties={
+            'name': fields.KeywordField(normalizer=lowercase_normalizer),
+        }
+    )
+
+    fk_relation_fields = [
+        'country', 'data_type', 'data_set', 'data_source', 'info_level', 'topic_category', 'resource_type',
+    ]
+
+    def get_queryset(self):
+        return super().get_queryset().select_related(
+            *self.fk_relation_fields
+        )
 
     class Meta:
         model = Document
         fields = [
             'id',
-            'description',
             'published_year',
             'data_collection_start_year',
             'data_collection_end_year',
             'next_update_year',
-            'additional_info'
         ]
+
+
+class DocSearch(FacetedSearch):
+    doc_types = [DocumentDoc]
+    fields = ['title', 'description']
+    facets = {
+        'country': TermsFacet(field='country'),
+        'data_type': TermsFacet(field='data_type'),
+        'data_set': TermsFacet(field='data_set'),
+        'data_source': TermsFacet(field='data_source'),
+        'info_level': TermsFacet(field='info_level'),
+        'topic_category': TermsFacet(field='topic_category'),
+        'resource_type': TermsFacet(field='resource_type'),
+        # TODO: Find a way to use nested facets
+        'keywords': TermsFacet(field='keywords.name'),
+        'nuts_levels': TermsFacet(field='nuts_levels.name')
+    }
