5b67d54962badb4b4cfc3a3154fc0e78cc5a8a3f
Andrei Duhnea <andrei@duhnea.net>
2018-06-20 10:27:31 +0300

    [refs. #96422] Add ES debug toolbar

--- nfi_search/urls.py
+++ nfi_search/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
+from rest_framework.documentation import include_docs_urls
 from .spabundle import spabundle
 from search import urls as api_urls
 
@@ -12,7 +13,8 @@ API_VERSION = '1'
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/%s/' % API_VERSION, include((api_urls, 'api'), namespace='api')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    url(r'^api-docs/', include_docs_urls(title='API Documentation', public=False)),
+  ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 if settings.DEBUG:
