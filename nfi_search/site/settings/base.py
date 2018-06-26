4adc4684b6287e69ae73641de026943f0a16a023
Andrei Duhnea <andrei@duhnea.net>
2018-06-26 13:24:19 +0300

    Add Django-extensions

--- nfi_search/site/settings/base.py
+++ nfi_search/site/settings/base.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'django_elasticsearch_dsl',
     'webpack_loader',
     'rest_framework',
