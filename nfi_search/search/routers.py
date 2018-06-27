fb7997d6728fa82bf4777dc3eecc3deb661c626a
Andrei Duhnea <andrei@duhnea.net>
2018-06-27 14:28:37 +0300

    [refs #96422] Apply URL prefix to facets router

--- nfi_search/search/routers.py
+++ nfi_search/search/routers.py
@@ -14,15 +14,15 @@ from .views import (
 
 
 facets_router = routers.SimpleRouter()
-facets_router.register('info-level', InfoLevelViewSet, base_name='info-level')
-facets_router.register('country', CountryViewSet, base_name='country')
-facets_router.register('data-type', DataTypeViewSet, base_name='data-type')
-facets_router.register('data-set', DataSetViewSet, base_name='data-set')
-facets_router.register('resource-type', ResourceTypeViewSet, base_name='resource-type')
-facets_router.register('topic-category', TopicCategoryViewSet, base_name='topic-category')
-facets_router.register('nuts-level', NUTSLevelViewSet, base_name='nuts-level')
-facets_router.register('keyword', KeywordViewSet, base_name='keyword')
-facets_router.register('language', LanguageViewSet, base_name='language')
+facets_router.register('facets/info-level', InfoLevelViewSet, base_name='info-level')
+facets_router.register('facets/country', CountryViewSet, base_name='country')
+facets_router.register('facets/data-type', DataTypeViewSet, base_name='data-type')
+facets_router.register('facets/data-set', DataSetViewSet, base_name='data-set')
+facets_router.register('facets/resource-type', ResourceTypeViewSet, base_name='resource-type')
+facets_router.register('facets/topic-category', TopicCategoryViewSet, base_name='topic-category')
+facets_router.register('facets/nuts-level', NUTSLevelViewSet, base_name='nuts-level')
+facets_router.register('facets/keyword', KeywordViewSet, base_name='keyword')
+facets_router.register('facets/language', LanguageViewSet, base_name='language')
 
 
 search_router = routers.SimpleRouter()
