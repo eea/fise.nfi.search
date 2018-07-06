092e9485694df044b93f4e51a89039073ea43ca6
Andrei Duhnea <andrei@duhnea.net>
2018-07-06 13:25:44 +0300

    WIP containerization updates

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,7 @@ if [ -z "$POSTGRES_HOST" ]; then
     POSTGRES_HOST="postgres"
 fi
 
-while ! nc -z $POSTGRES_HOST 5432; do
+while ! nc -z ${POSTGRES_HOST} 5432; do
   echo "Waiting for PostgreSQL server at '$POSTGRES_HOST' to accept connections on port 5432..."
   sleep 1s
 done
@@ -32,11 +32,11 @@ case "$1" in
             exec python manage.py runserver 0.0.0.0:${GUNICORN_PORT:-8000}
         else
             exec gunicorn -e SCRIPT_NAME=$SCRIPT_NAME \
-                        nfi_search.wsgi:application \
+                        nfi_search.site.wsgi:application \
                         --name search \
                         --bind 0.0.0.0:${GUNICORN_PORT:-8000} \
                         --workers 3 \
-                        --timeout $TIMEOUT \
+                        --timeout ${TIMEOUT} \
                         --access-logfile - \
                         --error-logfile -
         fi
