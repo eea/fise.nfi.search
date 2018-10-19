690e5400f1b44b0c5f50c09dfbcfcf2bb35cd7ad
Andrei Duhnea <andrei@duhnea.net>
2018-10-19 13:30:37 +0300

    Configurable ES index name

--- nfi_search/search/documents.py
+++ nfi_search/search/documents.py
@@ -4,114 +4,91 @@ from elasticsearch_dsl.analysis import analyzer, normalizer, char_filter
 from .models import Document
 
 
-nfi = Index('nfi')
+nfi = Index(settings.ELASTICSEARCH_INDEX)
 nfi.settings(max_result_window=settings.MAX_RESULT_WINDOW)
 
 lowercase_normalizer = normalizer(
-    type='custom',
-    name_or_instance='lowercase_normalizer',
+    type="custom",
+    name_or_instance="lowercase_normalizer",
     char_filter=[],
-    filter='lowercase',
+    filter="lowercase",
 )
 
 no_digits_char_filter = char_filter(
-    name_or_instance='no_digits',
-    type='pattern_replace',
-    pattern='(\\d+)',
-    replace=''
+    name_or_instance="no_digits", type="pattern_replace", pattern="(\\d+)", replace=""
 )
 
 no_digits_analyzer = analyzer(
-    name_or_instance='no_digits',
-    tokenizer='standard',
-    filter=['standard', 'lowercase', 'stop'],
-    char_filter=[no_digits_char_filter]
+    name_or_instance="no_digits",
+    tokenizer="standard",
+    filter=["standard", "lowercase", "stop"],
+    char_filter=[no_digits_char_filter],
 )
 
 
 @nfi.doc_type
 class DocumentDoc(DocType):
 
-    title = fields.TextField(
-        analyzer='standard',
-        boost=5.0,
-        fielddata=True,
-    )
+    title = fields.TextField(analyzer="standard", boost=5.0, fielddata=True)
 
-    description = fields.TextField(
-        analyzer='standard',
-        boost=2.0,
-        fielddata=True,
-    )
+    description = fields.TextField(analyzer="standard", boost=2.0, fielddata=True)
 
-    text = fields.TextField(
-        analyzer=no_digits_analyzer,
-        fielddata=True,
-    )
+    text = fields.TextField(analyzer=no_digits_analyzer, fielddata=True)
 
     countries = fields.NestedField(
-        properties={
-            'name': fields.KeywordField(normalizer=lowercase_normalizer),
-        }
+        properties={"name": fields.KeywordField(normalizer=lowercase_normalizer)}
     )
 
     data_type = fields.KeywordField(
-        attr='data_type.name',
-        normalizer=lowercase_normalizer
+        attr="data_type.name", normalizer=lowercase_normalizer
     )
 
     data_set = fields.KeywordField(
-        attr='data_set.name',
-        normalizer=lowercase_normalizer
+        attr="data_set.name", normalizer=lowercase_normalizer
     )
 
     data_source = fields.KeywordField(
-        attr='data_source.name',
-        normalizer=lowercase_normalizer
+        attr="data_source.name", normalizer=lowercase_normalizer
     )
 
     info_level = fields.KeywordField(
-        attr='info_level.name',
-        normalizer=lowercase_normalizer
+        attr="info_level.name", normalizer=lowercase_normalizer
     )
 
     resource_type = fields.KeywordField(
-        attr='resource_type.name',
-        normalizer=lowercase_normalizer
+        attr="resource_type.name", normalizer=lowercase_normalizer
     )
 
     topic_category = fields.KeywordField(
-        attr='topic_category.name',
-        normalizer=lowercase_normalizer
+        attr="topic_category.name", normalizer=lowercase_normalizer
     )
 
     keywords = fields.NestedField(
-        properties={
-            'name': fields.KeywordField(normalizer=lowercase_normalizer),
-        }
+        properties={"name": fields.KeywordField(normalizer=lowercase_normalizer)}
     )
 
     nuts_levels = fields.NestedField(
-        properties={
-            'name': fields.KeywordField(normalizer=lowercase_normalizer),
-        }
+        properties={"name": fields.KeywordField(normalizer=lowercase_normalizer)}
     )
 
     fk_relation_fields = [
-        'data_type', 'data_set', 'data_source', 'info_level', 'topic_category', 'resource_type',
+        "data_type",
+        "data_set",
+        "data_source",
+        "info_level",
+        "topic_category",
+        "resource_type",
     ]
 
     def get_queryset(self):
-        return super().get_queryset().select_related(
-            *self.fk_relation_fields
-        )
+        return super().get_queryset().select_related(*self.fk_relation_fields)
 
     class Meta:
         model = Document
         fields = [
-            'id',
-            'published_year',
-            'data_collection_start_year',
-            'data_collection_end_year',
-            'next_update_year',
+            "id",
+            "published_year",
+            "data_collection_start_year",
+            "data_collection_end_year",
+            "next_update_year",
         ]
--- nfi_search/site/settings/base.py
+++ nfi_search/site/settings/base.py
@@ -4,7 +4,7 @@ from getenv import env
 from django.utils.log import DEFAULT_LOGGING
 
 
-def split_env(var, sep=',', default='', required=False):
+def split_env(var, sep=",", default="", required=False):
     value = env(var, default, required)
     return [e.strip() for e in value.split(sep) if e.strip()]
 
@@ -17,59 +17,59 @@ ROOT_DIR = BASE_DIR.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY', 'secret')
+SECRET_KEY = env("SECRET_KEY", "secret")
 
 SILENCED_SYSTEM_CHECKS = ["fields.W342"]
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG', False)
+DEBUG = env("DEBUG", False)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', *split_env('ALLOWED_HOSTS')]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", *split_env("ALLOWED_HOSTS")]
 
 # Application definition
 
 INSTALLED_APPS = [
-    'nfi_search.search.apps.Config',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django_extensions',
-    'django_elasticsearch_dsl',
-    'webpack_loader',
-    'rest_framework',
+    "nfi_search.search.apps.Config",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django_extensions",
+    "django_elasticsearch_dsl",
+    "webpack_loader",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
-CORS_ORIGIN_WHITELIST = split_env('CORS_ORIGIN_WHITELIST')
+CORS_ORIGIN_WHITELIST = split_env("CORS_ORIGIN_WHITELIST")
 
-ROOT_URLCONF = 'nfi_search.site.urls'
+ROOT_URLCONF = "nfi_search.site.urls"
 
-WSGI_APPLICATION = 'nfi_search.site.wsgi.application'
+WSGI_APPLICATION = "nfi_search.site.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': env('POSTGRES_HOST', 'postgres'),
-        'NAME': env('POSTGRES_DBNAME', 'nfi'),
-        'USER': env('POSTGRES_DBUSER', 'nfi'),
-        'PASSWORD': env('POSTGRES_DBPASS', 'nfi'),
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": env("POSTGRES_HOST", "postgres"),
+        "NAME": env("POSTGRES_DBNAME", "nfi"),
+        "USER": env("POSTGRES_DBUSER", "nfi"),
+        "PASSWORD": env("POSTGRES_DBPASS", "nfi"),
+        "PORT": "5432",
     }
 }
 
@@ -79,32 +79,24 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer',
-    ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20,
+    "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 20,
 }
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -113,108 +105,105 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_ROOT = ROOT_DIR / 'static'
-STATIC_URL = '/static/'
+STATIC_ROOT = ROOT_DIR / "static"
+STATIC_URL = "/static/"
 
-FIXTURE_DIRS = [ROOT_DIR / 'data' / 'fixtures']
+FIXTURE_DIRS = [ROOT_DIR / "data" / "fixtures"]
 
-_WEBPACK_DIST_DIR = ROOT_DIR / 'frontend' / 'dist'
+_WEBPACK_DIST_DIR = ROOT_DIR / "frontend" / "dist"
 
-_WEBPACK_BUILD_DIR = _WEBPACK_DIST_DIR / 'build'
+_WEBPACK_BUILD_DIR = _WEBPACK_DIST_DIR / "build"
 if _WEBPACK_BUILD_DIR.is_dir():
     STATICFILES_DIRS = (_WEBPACK_BUILD_DIR,)
 
 WEBPACK_LOADER = {
-    'DEFAULT': {
-        'CACHE': not DEBUG,
+    "DEFAULT": {
+        "CACHE": not DEBUG,
         # 'BUNDLE_DIR_NAME': 'webpack_bundles/',  # must end with slash
-        'BUNDLE_DIR_NAME': '',
-        'STATS_FILE': _WEBPACK_DIST_DIR / 'stats.json',
-        'POLL_INTERVAL': 0.1,
-        'TIMEOUT': None,
-        'IGNORE': ['.+\.hot-update.js', '.+\.map']
+        "BUNDLE_DIR_NAME": "",
+        "STATS_FILE": _WEBPACK_DIST_DIR / "stats.json",
+        "POLL_INTERVAL": 0.1,
+        "TIMEOUT": None,
+        "IGNORE": [".+\.hot-update.js", ".+\.map"],
     }
 }
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ROOT_DIR / 'templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [ROOT_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ]
         },
-    },
+    }
 ]
 
 
 ELASTICSEARCH_DSL = {
-    'default': {
-        'hosts': env('ELASTICSEARCH_HOST'),
-        'http_auth': env('ELASTICSEARCH_AUTH'),
-        'timeout': env('ELASTICSEARCH_TIMEOUT', 10)
-    },
+    "default": {
+        "hosts": env("ELASTICSEARCH_HOST"),
+        "http_auth": env("ELASTICSEARCH_AUTH"),
+        "timeout": env("ELASTICSEARCH_TIMEOUT", 10),
+    }
 }
+
+ELASTICSEARCH_INDEX = env("ELASTICSEARCH_INDEX", "nfi")
 
 MAX_RESULT_WINDOW = 10000  # This is ElasticSearch's default, but we define it
 # here explicitly to minimize refactoring in case we ever change it.
 
 
 # Imported files directory
-FILES_DIR = env('FILES_DIR', required=True)
+FILES_DIR = env("FILES_DIR", required=True)
 
 # Path of files to be imported
-IMPORT_FILES_DIR = env('IMPORT_FILES_DIR')
+IMPORT_FILES_DIR = env("IMPORT_FILES_DIR")
 
-TIKA_HOST = env('TIKA_HOST')
-TIKA_PORT = env('TIKA_PORT', default=9998)
-TIKA_URL = f'http://{TIKA_HOST}:{TIKA_PORT}'
-TIKA_TIMEOUT = env('TIKA_TIMEOUT', 10)
+TIKA_HOST = env("TIKA_HOST")
+TIKA_PORT = env("TIKA_PORT", default=9998)
+TIKA_URL = f"http://{TIKA_HOST}:{TIKA_PORT}"
+TIKA_TIMEOUT = env("TIKA_TIMEOUT", 10)
 
-NFI_SEARCH_USE_TLS = env('NFI_SEARCH_USE_TLS', False)
-NFI_SEARCH_DOMAIN = env('NFI_SEARCH_DOMAIN')
+NFI_SEARCH_USE_TLS = env("NFI_SEARCH_USE_TLS", False)
+NFI_SEARCH_DOMAIN = env("NFI_SEARCH_DOMAIN")
 
 
 LOGGING_CONFIG = None
-LOGLEVEL = env('LOGLEVEL', 'info').upper()
-ES_LOGLEVEL = env('ES_LOGLEVEL', 'notset').upper()
-logging.config.dictConfig({
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'console': {
-            'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+LOGLEVEL = env("LOGLEVEL", "info").upper()
+ES_LOGLEVEL = env("ES_LOGLEVEL", "notset").upper()
+logging.config.dictConfig(
+    {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "console": {
+                "format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s"
+            },
+            "django.server": DEFAULT_LOGGING["formatters"]["django.server"],
         },
-        'django.server': DEFAULT_LOGGING['formatters']['django.server'],
-    },
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-            'formatter': 'console',
+        "handlers": {
+            "console": {"class": "logging.StreamHandler", "formatter": "console"},
+            "django.server": DEFAULT_LOGGING["handlers"]["django.server"],
         },
-        'django.server': DEFAULT_LOGGING['handlers']['django.server'],
-    },
-    'loggers': {
-        '': {
-            'level': 'WARNING',
-            'handlers': ['console'],
+        "loggers": {
+            "": {"level": "WARNING", "handlers": ["console"]},
+            "nfi_search": {
+                "level": LOGLEVEL,
+                "handlers": ["console"],
+                "propagate": False,
+            },
+            "elasticsearch.trace": {
+                "level": ES_LOGLEVEL,
+                "handlers": ["console"],
+                "propagate": False,
+            },
+            "django.server": DEFAULT_LOGGING["loggers"]["django.server"],
         },
-        'nfi_search': {
-            'level': LOGLEVEL,
-            'handlers': ['console'],
-            'propagate': False,
-        },
-        'elasticsearch.trace': {
-            'level': ES_LOGLEVEL,
-            'handlers': ['console'],
-            'propagate': False,
-        },
-        'django.server': DEFAULT_LOGGING['loggers']['django.server'],
-    },
-})
-
+    }
+)
